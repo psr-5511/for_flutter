f47c10d6c7858e366f7d365bc5252cae77628dad
Pranay Sharma <pranay07417@gmail.com>
2022-02-27 12:12:43 +0530

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Home_Page extends StatelessWidget {
+  const Home_Page({Key? key}) : super(key: key);
+
+  final int days = 30;
+  final String name = "Codepur";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Text('Welocme to $days of flutter by $name'),
+        ),
+      ),
+    );
+  }
+}
